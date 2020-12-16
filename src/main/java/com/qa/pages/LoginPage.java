c3b3ef682209fdaad34a0b2e3f3ce27aee5d8d65
itskp7 <rogkp2906@gmail.com>
2020-12-15 23:31:03 -0500

    added search page and modified login page

--- src/main/java/com/qa/pages/LoginPage.java
+++ src/main/java/com/qa/pages/LoginPage.java
@@ -3,7 +3,11 @@ package com.qa.pages;
 public class LoginPage {
 	
 	public void loginPage() {
-		System.out.println(" login page");
+		System.out.println("login page");
+	}
+	
+	public void loginSet() {
+		System.out.println("login page");
 	}
 
 }
